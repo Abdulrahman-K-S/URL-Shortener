057d43f8343e94929315b3bb8e24199177e5eba4
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-09-03 21:54:07 +0300

    feat: add URL model
    This model contains the basic necessary attributes for the URL to be able to be mapped from it's original URL to the shortened URL.
    
    The model contains the URL id, original_url, shortened_url, and the creation timestamp of the url.
    
    Files:
    - app/models/__init__.py
    - app/models/url.py

---
+++ app/models/url.py
@@ -0,0 +1,23 @@
+from app import db
+import datetime
+import hashlib
+
+class URL(db.Model):
+    """URL
+
+    This class contains the attributes for the URL to be able
+    to map the shortened url to it's original one
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    original_url = db.Column(db.String(2048), nullable=False)
+    shortened_url = db.Column(db.String(64), nullable=False)
+    created_at = db.Column(db.Datetime, default=datetime.datetime.now)
+
+    def __init__(self, original_url):
+        """__init__
+        """
+        self.original_url = original_url
+        self.shortened_url = self.generate_shortened_url()
+    
+    def generate_shortened_url(self):
+        return hashlib.md5(self.original_url.encode()).hexdigest()[:128]
