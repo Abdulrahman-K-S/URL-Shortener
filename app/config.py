06328b3716f2716ff4daa051a270948cb35b1275
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-09-06 13:49:44 +0300

    feat: add MySQL url connection
    Added the MySQL url connection

--- app/config.py
+++ app/config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///urls.db')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql+pymysql://localhost/URLShortener')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
