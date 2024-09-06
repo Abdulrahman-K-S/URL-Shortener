d8ec0fe50eb552145329e0ae2e8ab85ce1dd03ff
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-09-06 14:16:28 +0300

    feat: add ability to redirect to original url
    Now the api can store the url in a shortened form and if the shortened url is called it will redirect to the original one

--- app/controller/url_controller.py
+++ app/controller/url_controller.py
@@ -1,6 +1,6 @@
 from app import db
 from ..service import save_url, get_original_url
-from flask import url_for
+from flask import url_for, redirect
 
 def create_short_url(original_url):
     if not original_url:
@@ -12,3 +12,9 @@ def create_short_url(original_url):
     
     new_url = save_url(original_url)
     return url_for('create', shortened_path=new_url.shortened_url, _external=True)
+
+def redirect_to_original(shortened_url):
+    original_url = get_original_url(shortened_url=shortened_url)
+    if original_url:
+        return redirect(original_url.original_url)
+    return 'URL not found', 404
