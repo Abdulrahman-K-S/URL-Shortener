19ef70f9e3e5023be60ba77b07fffd9a8ddf91ca
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-09-12 18:42:26 +0300

    refactor: cleanup

--- app/controller/url_controller.py
+++ app/controller/url_controller.py
@@ -8,13 +8,13 @@ def create_short_url(original_url):
 
     existing_url = get_original_url(original_url)
     if existing_url:
-        return url_for('create', shortened_path=existing_url.shortened_url, _external=True)
+        return url_for('create', shortened_url=existing_url.shortened_url, _external=True)
     
     new_url = save_url(original_url)
-    return url_for('create', shortened_path=new_url.shortened_url, _external=True)
+    return url_for('create', shortened_url=new_url.shortened_url, _external=True)
 
 def redirect_to_original(shortened_url):
-    original_url = get_original_url(shortened_url=shortened_url)
-    if original_url:
-        return redirect(original_url.original_url)
+    url = get_original_url(shortened_url=shortened_url)
+    if url:
+        return redirect(url.original_url)
     return 'URL not found', 404
